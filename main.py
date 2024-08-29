fc71baebaf5d0583e8a159b2185b60ce8c19a337
kozukidaichi <kozuki.daichi.fm@tut.jp>
2024-08-29 10:45:13 +0900

    main try 3

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from test import test
+from test1 import test
 
 def main():
     with open("test/input.txt", "r", encoding="utf-8") as f:
         for i, line in enumerate(f):
             in_txt = line.strip()
-            out_txt = test(in_txt)
+            for i in range(3):
+                try:
+                    out_txt = test(in_txt)
 
-            with open(f"out/output_{i}.txt", "a", encoding="utf-8") as f:
-                f.write(in_txt + "\n\n")
-                f.write(out_txt + "\n")
+                    with open(f"out/output_{i}.txt", "a", encoding="utf-8") as f:
+                        f.write(in_txt + "\n\n")
+                        f.write(out_txt + "\n")
+                    break
+                except:
+                    pass
 
+main()
